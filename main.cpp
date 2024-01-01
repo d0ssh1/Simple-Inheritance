50f6f4f912e0bd4bd483fb52627e9c1590c54cec
d0ssh1 <109642808+d0ssh1@users.noreply.github.com>
2024-01-01 17:02:23 +1000

    Создание свойств для Human

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
-class Human{
+class Human {
 protected:
     int age;
     bool sex;
+public:
+    Human(int a, bool s) : age(a), sex(s) {}
 };
 
 
 int main() {
-    Human Artem;
-    Human Eugene;
+    Human Artem(19,1);
+    Human Eugene(19,1);
+    Human Vika(19,0);
     return 0;
 }
